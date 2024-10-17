ea503bcb6ad1d574bd6e84acf3c8040c085891d1
MayconBan <mayconguilherme040@gmail.com>
2024-10-17 16:51:24 -0300

    Update Calculator.py
    I found a way for it to repeat itself over and over again

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,5 @@
+import time
+
 def adicao():
     print(x + y)
 def subtracao():
@@ -10,25 +12,28 @@ def divisao():
     else:
         print(x / y)
 
+while True:
+    print('--------------------------------------------------------------')
+    print('Ola seja bem vindo a minha calculadora ')
+    print('Após o calculo em 5 segundos sera aberta uma nova calculadora ')
+    print('Qual operação deseja realizar')
+    print('1 - Adição \n2 - Subtração \n3 - Multiplicação \n4 - Divisão ')
+    print('-------------------------------------------------------------')
 
-print('---------------------------------------')
-print('Ola seja bem vindo a minha calculadora ')
-print('Qual operação deseja realizar')
-print('1 - Adição \n2 - Subtração \n3 - Multiplicação \n4 - Divisão ')
-print('---------------------------------------')
+    x = float(input('Digite o primeiro numero - '))
+    y = float(input('Digite o segundo numeoro - '))
 
-x = float(input('Digite o primeiro numero - '))
-y = float(input('Digite o segundo numeoro - '))
+    n = int(input('Digite o numero correpondente a operação - '))
 
-n = int(input('Digite o numero correpondente a operação - '))
-
-if n == 1:
-    adicao()
-elif n == 2:
-    subtracao()
-elif n == 3:
-    multiplicacao()
-elif n == 4:
-    divisao()
-else:
-    print('Por favor digite um numero que correponda a uma operação')
+    if n == 1:
+        adicao()
+    elif n == 2:
+        subtracao()
+    elif n == 3:
+        multiplicacao()
+    elif n == 4:
+        divisao()
+    else:
+        print('Por favor digite um numero que correponda a uma operação')
+    
+    time.sleep(5)
